f38872d2c13b5b5ba350273b6308808af7bf1a44
sayande717 <sayande717@github.com>
2024-01-27 18:53:41 +0530

    start nvim on startup

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,5 +1,9 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+local function open_nvim_terminal()
+    require("nvterm.terminal").new "horizontal"
+end
+
 local function open_nvim_tree()
   -- open the tree
   require("nvim-tree.api").tree.open()
@@ -11,5 +15,6 @@ autocmd("VimResized", {
   command = "tabdo wincmd =",
 })
 
+autocmd({ "VimEnter"}, { callback = open_nvim_terminal })
 autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
